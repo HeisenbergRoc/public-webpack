8f1d0b647e6f255002be595e995e066448780215
HeisenbergRoc <18700840614@126.com>
2021-03-23 22:17:23 +0800

    base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
   // console.log(baseConfig);
 
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'D:/WorkSpace/webpack-step/chapter-build/test/smoke/template/src/index/index.js')
-    assert.equal(baseConfig.entry.search, 'D:/WorkSpace/webpack-step/chapter-build/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('public-build/test/smoke/template/src/index/index.js') > -1, true)
+    assert.equal(baseConfig.entry.search.indexOf('public-build/test/smoke/template/src/search/index.js') > -1, true)
   })
 })
